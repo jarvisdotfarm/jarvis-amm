a95fb0cbd7f1acaedb6ddcc86edd3aac868dcb1d
Jarvis.Farm <81905160+jarvisdotfarm@users.noreply.github.com>
2021-04-08 15:02:29 +0700

    Add files via upload

---
+++ src/constants/eggPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0xE1ADce5e647DdF99f3525700825b5f1Ce00849BF',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0xE1ADce5e647DdF99f3525700825b5f1Ce00849BF'
+}
+
+export default priceContracts
